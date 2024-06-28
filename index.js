c80f366f7e471909c7ba4be603ec4163500a2565
Bogdan <chioarub@gmail.com>
2024-06-28 18:54:10 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/test', (req, res) => {
+    console.log('----->>>>>', req.query);
+    res.send('Hello World Claudiu!')
+});
+
+app.listen(9999, 'localhost', () => {
+    console.log('*** APP STARTED **');
+})
